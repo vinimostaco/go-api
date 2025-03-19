77973d231532608aec590899104ebfb552592530
viniciusmostacoalest <vini.mostaco@hotmail.com>
2025-03-19 12:21:32 -0300

    chore: consume mocked controller

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"go-api/controller"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main(){
 	server := gin.Default()
+
+	ProductController := controller.NewProductController()
 
 	server.GET("/ping", func(ctx *gin.Context){
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/products", ProductController.GetProducts)
 
 	server.Run(":8000")
 }
