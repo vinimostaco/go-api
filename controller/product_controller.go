ccd47235942372b62c4c82174b257ab5d9b8594d
viniciusmostacoalest <vini.mostaco@hotmail.com>
2025-03-19 12:21:06 -0300

    feat: create product controller with mock

---
+++ controller/product_controller.go
@@ -0,0 +1,29 @@
+package controller
+
+import (
+	"go-api/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type productController struct{
+	//use case
+}
+
+func NewProductController() productController{
+	return productController{}
+}
+
+func(p *productController) GetProducts(ctx *gin.Context){
+
+	products := []model.Product{
+		{
+			ID: 1,
+			Name: "Batata frita",
+			Price: 20,
+		},
+	}
+
+	ctx.JSON(http.StatusOK, products)
+}
